5da45f2abcc2130bf4cb93e009cac149dbd2caa9
yakabuff <yekeb00f@gmail.com>
2023-06-04 00:29:50 -0400

    made load comment/submission scripts more robust

--- scripts/load_comments.py
+++ scripts/load_comments.py
@@ -15,15 +15,68 @@ with open(filename) as file:
   for line in file:
       line = line.rstrip()
       com_dict = json.loads(line)
-      identifier = com_dict['id'].strip()
-      subreddit = com_dict['subreddit'].strip()
-      author = com_dict['author'].strip()
-      score = com_dict['score']
-      gilded = com_dict['gilded']
-      created_utc = com_dict['created_utc']
-      body = com_dict['body'].strip().replace("\u0000", "")
-      parent_id = com_dict['parent_id'].strip()
-      link_id = com_dict['link_id'].strip()
+
+      if 'id' in com_dict and isinstance(com_dict['id'], str):
+        identifier = com_dict['id'].strip()
+      else:
+        print("error: could not find ID")
+        print("================================")
+        cursor.execute('ROLLBACK;')
+        conn.close()
+        sys.exit(1)
+
+      if 'subreddit' in com_dict and isinstance(com_dict['subreddit'], str):
+        subreddit = com_dict['subreddit'].strip()
+      else:
+        print("error: could not find subreddit for identifier " + identifier)
+        print("================================")
+        cursor.execute('ROLLBACK;')
+        conn.close()
+        sys.exit(1)
+
+      if 'author' in com_dict and isinstance(com_dict['author'], str):
+        author = com_dict['author'].strip()
+      else:
+        print("warning: could not find author in " + identifier)
+        author = "[unknown]"
+      
+      if 'score' in com_dict:
+        score = com_dict['score']
+      else:
+        print("warning: could not find score in " + identifier)
+        score = 0
+      
+      if 'gilded' in com_dict:
+        gilded = com_dict['gilded']
+      else:
+        print("warning: could not find gilded in " + identifier)
+        gilded = 0
+
+      if 'created_utc' in com_dict:
+        created_utc = com_dict['created_utc']
+      else:
+        print("warning: could not find created_utc in " + identifier)
+        created_utc = 0
+
+      if 'body' in com_dict and isinstance(com_dict['body'], str):
+        # delete null chars
+        body = com_dict['body'].strip().replace("\u0000", "")
+      else:
+        print("warning: could not find body in " + identifier)
+        body = ""
+
+      if 'link_id' in com_dict and isinstance(com_dict['link_id'], str):
+        link_id = com_dict['link_id'].strip()
+      else:
+        print("warning: could not find link_id in " + identifier)
+        link_id = ""
+
+      if 'parent_id' in com_dict and isinstance(com_dict['parent_id'], str):
+        parent_id = com_dict['parent_id'].strip()
+      else:
+        print("warning: could not find parent_id in " + identifier)
+        parent_id = link_id
+
       print('====================')
       try:
         cursor.execute('INSERT INTO comments(id, subreddit, body, author, score, gilded, created_utc, parent_id, link_id) VALUES (%s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s) ON CONFLICT (id) DO NOTHING', [identifier, subreddit, body, author, score, gilded, created_utc, parent_id, link_id])
--- scripts/load_sub.py
+++ scripts/load_sub.py
@@ -15,25 +15,98 @@ with open(filename) as file:
   for line in file:
       line = line.rstrip()
       sub_dict = json.loads(line)
-      if 'name' in sub_dict:
+      # Rollback if id or subreddit not found.  Data is essentially useless
+      # id is sometimes called name in dumps
+      if 'id' in sub_dict and isinstance(sub_dict['id'], str):
+        identifier = sub_dict['id'].strip()
+      elif 'name' in sub_dict and isinstance(sub_dict['name'], str):
         identifier = sub_dict['name'].strip()
+        identifier = identifier.split('_')[1]
       else:
-          identifier = sub_dict['id'].strip()
-      subreddit = sub_dict['subreddit'].strip()
-      title = sub_dict['title'].strip()
-      author = sub_dict['author'].strip()
-      permalink = sub_dict['permalink'].strip()
-      thumbnail = sub_dict['thumbnail'].strip()
-      num_comments = sub_dict['num_comments']
-      if 'url' in sub_dict and sub_dict['url'] != None:
+        print("error: could not find ID")
+        print("================================")
+        cursor.execute('ROLLBACK;')
+        conn.close()
+        sys.exit(1)
+
+      if 'subreddit' in sub_dict and isinstance(sub_dict['subreddit'], str):
+        subreddit = sub_dict['subreddit'].strip()
+      else:
+        print("error: could not find subreddit for: " + identifier)
+        print("================================")
+        cursor.execute('ROLLBACK;')
+        conn.close()
+        sys.exit(1)
+
+      if 'title' in sub_dict and isinstance(sub_dict['title'], str):
+        title = sub_dict['title'].strip()      
+      else:
+        print("warning: could not find title for: " + identifier)
+        title = ""
+
+      if 'author' in sub_dict and isinstance(sub_dict['author'], str):
+        author = sub_dict['author'].strip()
+      else:
+        print("warning: could not find author in " + identifier)
+        author = "[unknown]"
+
+      if 'permalink' in sub_dict and isinstance(sub_dict['permalink'], str):
+        permalink = sub_dict['permalink'].strip()
+      else:
+        print("warning: could not find permalink in " + identifier)
+        permalink = f'/r/{subreddit}/comments/{identifier}/foobar'
+
+      if 'num_comments' in sub_dict:
+        num_comments = sub_dict['num_comments']
+      else:
+        print("warning: could not find num_comments in " + identifier)
+        num_comments = 0
+
+      if 'url' in sub_dict and isinstance(sub_dict['url'], str):
         url = sub_dict['url'].strip()
       else:
-         url = permalink
-      score = sub_dict['score']
-      gilded = sub_dict['gilded']
-      created_utc = sub_dict['created_utc']
-      self_text = sub_dict['selftext'].strip()
-      is_self = sub_dict['is_self']
+        url = f'http://reddit.com/r/{subreddit}/comments/{identifier}/blah'
+
+      if 'score' in sub_dict:
+        score = sub_dict['score']
+      else:
+        print("warning: could not find score in " + identifier)
+        score = 0
+
+      if 'gilded' in sub_dict:
+        gilded = sub_dict['gilded']
+      else:
+        print("warning: could not find gilded in " + identifier)
+        gilded = 0
+
+      if 'created_utc' in sub_dict:
+        created_utc = sub_dict['created_utc']
+      else:
+        print("warning: could not find created_utc in " + identifier)
+        created_utc = 0
+
+      if 'selftext' in sub_dict and isinstance(sub_dict['selftext'], str):
+        self_text = sub_dict['selftext'].strip()
+      else:
+        print("warning: could not find selftext in " + identifier)
+        self_text = ""
+
+      if 'is_self' in sub_dict and isinstance(sub_dict['is_self'], bool):
+          is_self = sub_dict['is_self']
+      else:
+          print("warning: could not find is_self in " + identifier)
+          # Guess if self post
+          is_self = True if "reddit.com/r/" in url else False
+
+      if 'thumbnail' in sub_dict and isinstance(sub_dict['thumbnail'], str):
+        thumbnail = sub_dict['thumbnail'].strip()
+      else:
+        print("warning: could not find thumbnail in " + identifier)
+        if is_self:
+          thumbnail = "self"
+        else:
+          thumbnail = "default"
+
       print('====================')
       try:
         cursor.execute('INSERT INTO submissions(id, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s)', [identifier, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self])

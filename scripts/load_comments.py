6c9cf098240f93c96b5171bc5ed72b9701a80975
yakabuff <yekeb00f@gmail.com>
2023-06-06 02:05:20 -0400

    Revamped loading scripts

--- scripts/load_comments.py
+++ scripts/load_comments.py
@@ -1,6 +1,12 @@
 import json
 import psycopg2
 import sys
+import logging
+import datetime
+
+time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
+logging.basicConfig(filename='comments-'+time_now+'.log', encoding='utf-8', level=logging.DEBUG)
+
 filename = sys.argv[1]
 conn = psycopg2.connect(
   database="postgres",
@@ -9,92 +15,101 @@ conn = psycopg2.connect(
   host='localhost',
   port='5432'
 )
+line_number = 0
 cursor = conn.cursor()
-cursor.execute('BEGIN;')
+
 with open(filename) as file:
   for line in file:
+      line_number = line_number + 1
       line = line.rstrip()
       com_dict = json.loads(line)
 
       if 'id' in com_dict and isinstance(com_dict['id'], str):
         identifier = com_dict['id'].strip()
       else:
-        print("error: could not find ID")
-        print("================================")
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
+        logging.error("Could not find ID")
+        logging.debug("Line number: "+ str(line_number))
+        continue
 
       if 'subreddit' in com_dict and isinstance(com_dict['subreddit'], str):
         subreddit = com_dict['subreddit'].strip()
       else:
-        print("error: could not find subreddit for identifier " + identifier)
+        logging.error("Could not find subreddit for: " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         print("================================")
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
+        continue
 
       if 'author' in com_dict and isinstance(com_dict['author'], str):
         author = com_dict['author'].strip()
       else:
-        print("warning: could not find author in " + identifier)
+        logging.warning("Could not find author in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         author = "[unknown]"
       
-      if 'score' in com_dict:
+      if 'score' in com_dict and isinstance(com_dict['score'], int):
         score = com_dict['score']
+      elif 'score' in com_dict and isinstance(com_dict['score'], str) and com_dict['score'].isdigits():
+        score = int(com_dict['score'])
       else:
-        print("warning: could not find score in " + identifier)
+        logging.warning("Could not find score in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         score = 0
       
-      if 'gilded' in com_dict:
+      if 'gilded' in com_dict and isinstance(com_dict['gilded'], int):
         gilded = com_dict['gilded']
+      elif 'gilded' in com_dict and isinstance(com_dict['gilded'], str) and com_dict['gilded'].isdigits():
+        gilded = int(com_dict['gilded'])
       else:
-        print("warning: could not find gilded in " + identifier)
+        logging.warning("Could not find gilded in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         gilded = 0
 
-      if 'created_utc' in com_dict:
+      if 'created_utc' in com_dict and isinstance(com_dict['created_utc'], int):
         created_utc = com_dict['created_utc']
+      elif 'created_utc' in com_dict and isinstance(com_dict['created_utc'], str) and com_dict['created_utc'].isdigit():
+        created_utc = int(com_dict['created_utc'])
       else:
-        print("warning: could not find created_utc in " + identifier)
+        logging.error("Could not find created_utc in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         created_utc = 0
 
       if 'body' in com_dict and isinstance(com_dict['body'], str):
         # delete null chars
         body = com_dict['body'].strip().replace("\u0000", "")
       else:
-        print("warning: could not find body in " + identifier)
+        logging.warning("Could not find body in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         body = ""
 
       if 'link_id' in com_dict and isinstance(com_dict['link_id'], str):
         link_id = com_dict['link_id'].strip()
       else:
-        print("warning: could not find link_id in " + identifier)
+        logging.warning("Could not find link_id in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         link_id = ""
 
       if 'parent_id' in com_dict and isinstance(com_dict['parent_id'], str):
         parent_id = com_dict['parent_id'].strip()
       else:
-        print("warning: could not find parent_id in " + identifier)
+        logging.warning("Could not find parent_id in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         parent_id = link_id
 
       print('====================')
       try:
         cursor.execute('INSERT INTO comments(id, subreddit, body, author, score, gilded, created_utc, parent_id, link_id) VALUES (%s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s) ON CONFLICT (id) DO NOTHING', [identifier, subreddit, body, author, score, gilded, created_utc, parent_id, link_id])
       except Exception as error:
-        print("ERROR:" + str(error))
-        print(identifier)
-        print(subreddit)
-        print(body)
-        print(author)
-        print(score)
-        print(gilded)
-        print(created_utc)
-        print(parent_id)
-        print(link_id)
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
-
+        logging.error("ERROR:" + str(error))
+        logging.debug(identifier)
+        logging.debug(subreddit)
+        logging.debug(body)
+        logging.debug(author)
+        logging.debug(score)
+        logging.debug(gilded)
+        logging.debug(created_utc)
+        logging.debug(parent_id)
+        logging.debug(link_id)
+        continue
       print(f"Identifier inserted succesfully {identifier}")
 conn.commit()
 conn.close()
--- scripts/load_sub.py
+++ scripts/load_sub.py
@@ -1,6 +1,12 @@
 import json
 import psycopg2
 import sys
+import logging
+import datetime
+
+time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
+logging.basicConfig(filename='submission-'+time_now+'.log', encoding='utf-8', level=logging.DEBUG)
+
 filename = sys.argv[1]
 conn = psycopg2.connect(
   database="postgres",
@@ -9,99 +15,119 @@ conn = psycopg2.connect(
   host='localhost',
   port='5432'
 )
+line_number = 0
 cursor = conn.cursor()
-cursor.execute('BEGIN;')
+
 with open(filename) as file:
   for line in file:
+      line_number = line_number + 1
       line = line.rstrip()
       sub_dict = json.loads(line)
-      # Rollback if id or subreddit not found.  Data is essentially useless
-      # id is sometimes called name in dumps
+
+      # id is occasionally missing.  Use the 'name' field to derive id
       if 'id' in sub_dict and isinstance(sub_dict['id'], str):
-        identifier = sub_dict['id'].strip()
-      elif 'name' in sub_dict and isinstance(sub_dict['name'], str):
-        identifier = sub_dict['name'].strip()
+        identifier = sub_dict['id'].strip().replace("\u0000", "").lower()
+      elif 'name' in sub_dict and isinstance(sub_dict['name'], str) and len(identifier.split('_')) > 1:
+        identifier = sub_dict['name'].strip().replace("\u0000", "").lower()
         identifier = identifier.split('_')[1]
       else:
-        print("error: could not find ID")
+        logging.error("Could not find ID")
+        logging.debug("Line number: "+ str(line_number))
         print("================================")
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
+        continue
 
       if 'subreddit' in sub_dict and isinstance(sub_dict['subreddit'], str):
-        subreddit = sub_dict['subreddit'].strip()
+        subreddit = sub_dict['subreddit'].strip().replace("\u0000", "").lower()
       else:
-        print("error: could not find subreddit for: " + identifier)
+        logging.error("Could not find subreddit for: " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         print("================================")
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
+        continue
 
       if 'title' in sub_dict and isinstance(sub_dict['title'], str):
-        title = sub_dict['title'].strip()      
+        title = sub_dict['title'].strip().replace("\u0000", "")
       else:
-        print("warning: could not find title for: " + identifier)
+        logging.warning("Could not find title for: " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         title = ""
 
       if 'author' in sub_dict and isinstance(sub_dict['author'], str):
-        author = sub_dict['author'].strip()
+        author = sub_dict['author'].strip().replace("\u0000", "").lower()
       else:
-        print("warning: could not find author in " + identifier)
+        logging.warning("Could not find author in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         author = "[unknown]"
 
       if 'permalink' in sub_dict and isinstance(sub_dict['permalink'], str):
-        permalink = sub_dict['permalink'].strip()
+        permalink = sub_dict['permalink'].strip().replace("\u0000", "")
       else:
-        print("warning: could not find permalink in " + identifier)
+        logging.warning("Could not find permalink in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         permalink = f'/r/{subreddit}/comments/{identifier}/foobar'
 
-      if 'num_comments' in sub_dict:
+      if 'num_comments' in sub_dict and isinstance(sub_dict['num_comments'], int):
         num_comments = sub_dict['num_comments']
+      elif 'num_comments' in sub_dict and isinstance(sub_dict['num_comments'], str) and sub_dict['num_comments'].isdigits():
+        num_comments = int(sub_dict['num_comments'])
       else:
-        print("warning: could not find num_comments in " + identifier)
+        logging.warning("Could not find num_comments in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         num_comments = 0
 
       if 'url' in sub_dict and isinstance(sub_dict['url'], str):
-        url = sub_dict['url'].strip()
+        url = sub_dict['url'].strip().replace("\u0000", "")
       else:
         url = f'http://reddit.com/r/{subreddit}/comments/{identifier}/blah'
+        logging.warning("Could not find url in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
 
-      if 'score' in sub_dict:
+      if 'score' in sub_dict and isinstance(sub_dict['score'], int):
         score = sub_dict['score']
+      elif 'score' in sub_dict and isinstance(sub_dict['score'], str) and sub_dict['score'].isdigits():
+        score = int(sub_dict['score'])
       else:
-        print("warning: could not find score in " + identifier)
+        logging.warning("Could not find score in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         score = 0
 
-      if 'gilded' in sub_dict:
+      if 'gilded' in sub_dict and isinstance(sub_dict['gilded'], int):
         gilded = sub_dict['gilded']
+      elif 'gilded' in sub_dict and isinstance(sub_dict['gilded'], str) and sub_dict['gilded'].isdigits():
+        gilded = int(sub_dict['gilded'])
       else:
-        print("warning: could not find gilded in " + identifier)
+        logging.warning("Could not find gilded in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         gilded = 0
 
-      if 'created_utc' in sub_dict:
+      if 'created_utc' in sub_dict and isinstance(sub_dict['created_utc'], int):
         created_utc = sub_dict['created_utc']
+      elif 'created_utc' in sub_dict and isinstance(sub_dict['created_utc'], str) and sub_dict['created_utc'].isdigit():
+        created_utc = int(sub_dict['created_utc'])
       else:
-        print("warning: could not find created_utc in " + identifier)
+        logging.error("Could not find created_utc in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         created_utc = 0
 
       if 'selftext' in sub_dict and isinstance(sub_dict['selftext'], str):
-        self_text = sub_dict['selftext'].strip()
+        self_text = sub_dict['selftext'].strip().replace("\u0000", "")
       else:
-        print("warning: could not find selftext in " + identifier)
+        logging.warning("Could not find selftext in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         self_text = ""
 
       if 'is_self' in sub_dict and isinstance(sub_dict['is_self'], bool):
-          is_self = sub_dict['is_self']
+        is_self = sub_dict['is_self']
       else:
-          print("warning: could not find is_self in " + identifier)
-          # Guess if self post
-          is_self = True if "reddit.com/r/" in url else False
+        logging.warning("Could not find is_self in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
+        # Guess if self post
+        is_self = True if "reddit.com/r/" in url else False
 
       if 'thumbnail' in sub_dict and isinstance(sub_dict['thumbnail'], str):
-        thumbnail = sub_dict['thumbnail'].strip()
+        thumbnail = sub_dict['thumbnail'].strip().replace("\u0000", "")
       else:
-        print("warning: could not find thumbnail in " + identifier)
+        logging.warning("Could not find thumbnail in " + identifier)
+        logging.debug("Line number: "+ str(line_number))
         if is_self:
           thumbnail = "self"
         else:
@@ -109,13 +135,25 @@ with open(filename) as file:
 
       print('====================')
       try:
-        cursor.execute('INSERT INTO submissions(id, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s)', [identifier, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self])
+        cursor.execute('INSERT INTO submissions(id, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s), %s, %s) ON CONFLICT (id) DO NOTHING', [identifier, subreddit, title, author, permalink, thumbnail, num_comments, url, score, gilded, created_utc, self_text, is_self])
       except Exception as error:
-        print("ERROR: "+ str(error))
-        print("================================")
-        cursor.execute('ROLLBACK;')
-        conn.close()
-        sys.exit(1)
+        logging.error("ERROR: "+ str(error))
+        logging.debug("Line number: "+ str(line_number))
+        logging.debug(identifier)
+        logging.debug(subreddit)
+        logging.debug(title)
+        logging.debug(author)
+        logging.debug(permalink)
+        logging.debug(thumbnail)
+        logging.debug(num_comments)
+        logging.debug(url)
+        logging.debug(score)
+        logging.debug(gilded)
+        logging.debug(created_utc)
+        logging.debug(self_text)
+        logging.debug(is_self)
+        logging.debug("================================")
+        continue
       print(f"Identifier inserted succesfully {identifier}")
 conn.commit()
 conn.close()

8e314125fe74ab7155b73d4f644373b35aeb15c4
yakabuff <yekeb00f@gmail.com>
2023-07-24 02:07:31 -0400

    dockerize worker

---
+++ redarc_ingest/entry.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Generate .env with envars
+cd /ingest/redarc_ingest
+echo "INGEST_ENABLED=$INGEST_ENABLED" > .env
+cd /ingest/worker
+echo "PG_DATABASE=postgres
+PG_USER=postgres
+PG_PASSWORD=$PG_PASSWORD
+PG_HOST=$PG_HOST
+PG_PORT=$PG_PORT
+
+ES_USER=elastic
+ES_PASSWORD=$ES_PASSWORD
+ES_HOST=$ES_HOST
+ES_ENABLED=$ES_ENABLED
+
+CLIENT_ID=$CLIENT_ID
+CLIENT_SECRET=$CLIENT_SECRET
+PASSWORD=$PASSWORD
+USER_AGENT=$USER_AGENT
+REDDIT_USERNAME=$REDDIT_USERNAME" > .env
+
+cd /ingest
+
+redis-server &
+
+# Start gunicorn
+gunicorn -b 0.0.0.0:8000 --reload redarc_ingest.app &
+# Start reddit worker
+python3 -m worker.reddit_worker &
+
+# Start index worker
+python3 -m worker.index_worker &
+
+wait

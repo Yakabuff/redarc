4414b71fa659f94af0ed912a01b831d520547402
yakabuff <yekeb00f@gmail.com>
2023-08-31 02:14:33 -0400

    ingest: added more exception handling to index_worker

--- ingest/worker/index_worker.py
+++ ingest/worker/index_worker.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+import sys
 import time
 from dotenv import load_dotenv
 import logging
@@ -8,21 +9,24 @@ from psycopg2 import pool
 import psycopg2
 
 load_dotenv()
+try:
+   pg_pool = psycopg2.pool.SimpleConnectionPool(1, 20, user=os.getenv('PG_USER'),
+                                                         password=os.getenv('PG_PASSWORD'),
+                                                         host=os.getenv('PG_HOST'),
+                                                         port=os.getenv('PG_PORT'),
+                                                         database=os.getenv('PG_DATABASE'))
 
-pg_pool = psycopg2.pool.SimpleConnectionPool(1, 20, user=os.getenv('PG_USER'),
-                                                        password=os.getenv('PG_PASSWORD'),
-                                                        host=os.getenv('PG_HOST'),
-                                                        port=os.getenv('PG_PORT'),
-                                                        database=os.getenv('PG_DATABASE'))
+   pg_pool_fts = psycopg2.pool.SimpleConnectionPool(1, 20, user=os.getenv('PGFTS_USER'),
+                                                         password=os.getenv('PGFTS_PASSWORD'),
+                                                         host=os.getenv('PGFTS_HOST'),
+                                                         port=os.getenv('PGFTS_PORT'),
+                                                         database=os.getenv('PGFTS_DATABASE'))
 
-pg_pool_fts = psycopg2.pool.SimpleConnectionPool(1, 20, user=os.getenv('PGFTS_USER'),
-                                                        password=os.getenv('PGFTS_PASSWORD'),
-                                                        host=os.getenv('PGFTS_HOST'),
-                                                        port=os.getenv('PGFTS_PORT'),
-                                                        database=os.getenv('PGFTS_DATABASE'))
-
-pg_con = pg_pool.getconn()
-cursor = pg_con.cursor()
+   pg_con = pg_pool.getconn()
+   cursor = pg_con.cursor()
+except Exception as error:
+    logging.error(error)
+    sys.exit(1)
 
 def insert_search(_type, data):
    #Insert data into postgres fts

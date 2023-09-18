0684338799eabbab73a4a7f36038ff65b6371a6a
yakabuff <yakabuff@basedbin.org>
2023-09-18 15:26:43 -0400

    api: implemented rotating logs

--- api/redarc_logger.py
+++ api/redarc_logger.py
@@ -1,11 +1,18 @@
-import datetime
 import logging
+from logging.handlers import RotatingFileHandler
 import os
 
 def init_logger(name):
    if not os.path.exists('logs'):
       os.makedirs('logs')
    logger = logging.getLogger(name)
-   time_now  = datetime.datetime.now().strftime('%m_%d_%Y_%H_%M_%S') 
-   logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', filename='logs/redarc_api-'+time_now+'.log', encoding='utf-8', level=logging.DEBUG, datefmt='%Y-%m-%d %H:%M:%S')
+   filename ='logs/redarc_api.log'
+   handler = RotatingFileHandler(filename,
+                              maxBytes=1024*1024*50,
+                              backupCount=999)
+   logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        encoding='utf-8',
+                        level=logging.INFO,
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        handlers=[handler])
    return logger
